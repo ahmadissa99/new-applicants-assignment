d7d7e6d5d23f3f0dce77f59d16d7a976afe16152
ahmadissa99 <ahmad.issaa.999@gmail.com>
2023-11-01 22:29:39 +0100

    ahmadIssaCalc

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 // Add your JavaScript here
+let outputScreen = document.getElementById('outputScreen');
+function display(num) {
+    outputScreen.value += num;
+}
+
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    } catch (err) {
+        alert('invalid');
+    }
+}
+
+function ac() {
+    outputScreen.value = '';
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
